284e99e4c3772aaddca2868ab2ff2ea54b06c782
seranomi <2bumchan@naver.com>
2025-01-23 16:13:06 +0900

    refacto : 게시글 작성 기능 Repository 계층

--- src/boards/boards.repository.ts
+++ src/boards/boards.repository.ts
@@ -20,11 +20,27 @@ export class BoardRepository {
     const selectQuery = `SELECT * FROM board`;
     try {
       const [result] = await this.connectionPool.query(selectQuery);
-	  return result as Board[];
-    } catch (err){
-		throw new InternalServerErrorException('Database query failed', err);
-	}
+      return result as Board[];
+    } catch (err) {
+      throw new InternalServerErrorException('Database query failed', err);
+    }
   }
 
+  // 게시글 작성관련 데이터 액세스
+  async saveBoard(board: Board): Promise<string> {
+    const insertQuery = `INSERT INTO board(author, title, contents, status) VALUES (?, ?, ?, ?)`;
 
+    try {
+      const result = await this.connectionPool.query(insertQuery, [
+        board.author,
+        board.title,
+        board.contents,
+        board.status,
+      ]);
+      const message = 'Created success!';
+      return message;
+    } catch (err) {
+      throw new InternalServerErrorException('Database query failed', err);
+    }
+  }
 }
