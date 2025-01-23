02fd8088cd6505c6d6ffd318f1566d734ef73276
seranomi <2bumchan@naver.com>
2025-01-23 15:42:28 +0900

    refactor : Repository 계층 생성 및 서비스 코드 분리

---
+++ src/boards/boards.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { createPool, Pool } from 'mysql2/promise';
+import { databaseConfig } from 'src/configs/database.config';
+import { Board } from './boards.entity';
+
+@Injectable()
+export class BoardRepository {
+  private connectionPool: Pool;
+
+  constructor() {
+    this.connectionPool = createPool(databaseConfig);
+    this.connectionPool
+      .getConnection()
+      .then(() => console.log('DB Connected'))
+      .catch((err) => console.error('DB connection', err));
+  }
+
+  // 게시글 조회관련 데이터 액세스
+  async findAll(): Promise<Board[]> {
+    const selectQuery = `SELECT * FROM board`;
+    try {
+      const [result] = await this.connectionPool.query(selectQuery);
+	  return result as Board[];
+    } catch {
+		throw new InternalServerErrorException('Database query failed', err);
+	}
+  }
+
+  
+}
