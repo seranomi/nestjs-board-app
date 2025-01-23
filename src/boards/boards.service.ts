7e90deb7cf3404824a8cbc78ad93662ee106f507
seranomi <2bumchan@naver.com>
2025-01-23 15:46:34 +0900

    refactor : Service 계층의 async/await 비동기  Promise 반환으로 변경

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -8,36 +8,34 @@ import { Board } from './boards.entity';
 import { BoardStatus } from './boards-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
+import { BoardRepository } from './boards.repository';
 
 @Injectable()
 export class BoardsService {
-  // 데이터 베이스
-  private boards: Board[] = [];
+  // Repository 계층 DI
+  constructor(private boardsRepository: BoardRepository) {}
 
   // 모든 게시글 조회 기능
-  getAllBoards(): Board[] {
-    const foundBoards = this.boards;
-    if (foundBoards.length === 0) {
-      throw new NotFoundException('No boards found');
-    }
+  async getAllBoards(): Promise<Board[]> {
+    const foundBoards = await this.boardsRepository.findAll();
     return foundBoards;
   }
-  // 특정 게시글 조회 기능
-  getBoardDetailById(id: number): Board {
-    const foundBoard = this.boards.find((board) => board.id == id);
-    if (!foundBoard) {
-      throw new NotFoundException(`Board with ID ${id} not found`);
-    }
-    return foundBoard;
-  }
-  // 키워드(작성자)로 검색한 게시글 조회 기능
-  getBoardsByKeyword(author: string): Board[] {
-    const foundBoards = this.boards.filter((board) => board.author === author);
-    if (foundBoards.length === 0) {
-      throw new NotFoundException(`No boards found for author: ${author}`);
-    }
-    return foundBoards;
-  }
+  // // 특정 게시글 조회 기능
+  // getBoardDetailById(id: number): Board {
+  //   const foundBoard = this.boards.find((board) => board.id == id);
+  //   if (!foundBoard) {
+  //     throw new NotFoundException(`Board with ID ${id} not found`);
+  //   }
+  //   return foundBoard;
+  // }
+  // // 키워드(작성자)로 검색한 게시글 조회 기능
+  // getBoardsByKeyword(author: string): Board[] {
+  //   const foundBoards = this.boards.filter((board) => board.author === author);
+  //   if (foundBoards.length === 0) {
+  //     throw new NotFoundException(`No boards found for author: ${author}`);
+  //   }
+  //   return foundBoards;
+  // }
 
   // 게시글 생성 기능
   createBoard(createBoardDto: CreateBoardDto) {
@@ -59,28 +57,28 @@ export class BoardsService {
     }
   }
 
-  // 특정 번호의 게시글 수정
-  updateBoardById(id: number, updateBoardDto: UpdateBoardDto): Board {
-    const foundBoard = this.getBoardDetailById(id); // 여기서 이미 예외처리를 하고 있다
-    const { title, contents } = updateBoardDto;
-    foundBoard.title = title;
-    foundBoard.contents = contents;
-    return foundBoard;
-  }
-  // 특정 번호의 게시글 일부 수정
-  updateBoardStatusById(id: number, status: BoardStatus): Board {
-    const foundBoard = this.getBoardDetailById(id);
+  // // 특정 번호의 게시글 수정
+  // updateBoardById(id: number, updateBoardDto: UpdateBoardDto): Board {
+  //   const foundBoard = this.getBoardDetailById(id); // 여기서 이미 예외처리를 하고 있다
+  //   const { title, contents } = updateBoardDto;
+  //   foundBoard.title = title;
+  //   foundBoard.contents = contents;
+  //   return foundBoard;
+  // }
+  // // 특정 번호의 게시글 일부 수정
+  // updateBoardStatusById(id: number, status: BoardStatus): Board {
+  //   const foundBoard = this.getBoardDetailById(id);
 
-    // status는 PUBLIC PRIVATE 두 값중 하나만 갖는다.
+  //   // status는 PUBLIC PRIVATE 두 값중 하나만 갖는다.
     
-    foundBoard.status = status;
-    return foundBoard;
-  }
+  //   foundBoard.status = status;
+  //   return foundBoard;
+  // }
 
-  // 게시글 삭제 기능
-  deleteBoardById(id: number): void {
-    // 게시글 존재 여부 확인
-    const foundBoard = this.getBoardDetailById(id);
-    this.boards = this.boards.filter((board) => board.id != id);
-  }
+  // // 게시글 삭제 기능
+  // deleteBoardById(id: number): void {
+  //   // 게시글 존재 여부 확인
+  //   const foundBoard = this.getBoardDetailById(id);
+  //   this.boards = this.boards.filter((board) => board.id != id);
+  // }
 }
