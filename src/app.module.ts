1d1b9ec14d8f6394264fde5df4fe030f4c491adb
seranomi <2bumchan@naver.com>
2025-01-24 12:21:01 +0900

    chore : TypeORM 사용 기본 설정(config, module 등)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
-import { ConfigModule } from '@nestjs/config';
-import { databaseConfig } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
     BoardsModule
   ],
-  controllers: [],
-  providers: [
-    {
-      provide : 'DATABASE_CONFIG',
-      useValue : databaseConfig
-    }
-  ],
-  exports : ['DATABASE_CONFIG']
 })
 export class AppModule {}
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PW,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'], // 엔티티 파일의 위치
+  synchronize: true, // 개발중에만
+  logging: true,
+};
