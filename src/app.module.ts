4c604479b9951aad972da3613421b97bcf706b09
seranomi <2bumchan@naver.com>
2025-02-03 10:49:10 +0900

    feat : 회원가입 및 로그인 기능의 Auth 모듈 생성과 의존성 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    BoardsModule
+    BoardsModule,
+    AuthModule
   ],
 })
 export class AppModule {}
