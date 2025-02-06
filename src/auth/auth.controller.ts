d860aaf4f29a4317ea439585d4f386d52c305a0a
seranomi <2bumchan@naver.com>
2025-02-06 14:35:40 +0900

    refactor : arth.controller 로깅 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Logger, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
@@ -10,6 +10,7 @@ import { User } from './user.entity';
 
 @Controller('api/auth')
 export class AuthController {
+	private readonly logger = new Logger(AuthController.name); // Logger 인스턴스 생성
 	constructor(
 		private authService: AuthService,
 	){}
@@ -17,6 +18,7 @@ export class AuthController {
 	// 회원 가입 기능
 	@Post('/signup')
 	async createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto>{
+		this.logger.verbose(`Attempting to sign up usesr with email: ${createUserDto.email}`);
 		const userResponseDto = new UserResponseDto(await this.authService.signUp(createUserDto))
 		return userResponseDto;
 	}
@@ -24,6 +26,7 @@ export class AuthController {
 	// 로그인 기능
 	@Post('/signin')
 	async signIn(@Body() loginUserDto: LoginUserDto, @Res() res: Response): Promise<void>{
+		this.logger.verbose(`Attempting to sign in user with email: ${loginUserDto.email}`);
 		const accessToken = await this.authService.signIn(loginUserDto);
 
 		// [2] JWT를 쿠키에 저장
@@ -35,8 +38,9 @@ export class AuthController {
 	@Post('/test')
 	@UseGuards(AuthGuard('jwt'))
 	testForAuth(@GetUser() logginedUser: User) {
-		console.log(logginedUser); // 인증된 사용자의 정보 출력
-		console.log(logginedUser.id); // 인증된 사용자의 특정 필드 접근
+		this.logger.verbose(`Authenticated user accessing test route: ${logginedUser.email}`);
+		// console.log(logginedUser); // 인증된 사용자의 정보 출력
+		// console.log(logginedUser.id); // 인증된 사용자의 특정 필드 접근
 		return { message : 'Authenticated User', user: logginedUser};
 	}
 }
